ee6946e103480dec1c98f3749626fe0554fe8eed
user <user@gmail.com>
2023-01-09 09:51:03 +0100

    ex1 completado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Practica p = new Practica();
+            p.printArray(p.returnAparicions("frase de provaa az zz"));
     }
 }
--- src/Practica.java
+++ src/Practica.java
@@ -1,2 +1,41 @@
 public class Practica {
+    public int [][] returnAparicions(String frase){
+        final int abcLenght = 26;
+        final char fC = 'a';
+        final char lC = 'Z';
+        String combC;
+        String combCF;
+        int [][] seqAp= new int [abcLenght][abcLenght];
+        for (int i = 0 ; i < abcLenght;i++){
+            for (int e = 0; e < abcLenght;e++){
+               combC = ""+(char)(fC+i) +""+ (char)(fC+e);
+              for (int k = 0; k < frase.length()-1;k++){
+                  combCF="" + frase.charAt(k) +""+ frase.charAt(k+1);
+                  if (combCF.equals(combC)){
+                      seqAp[i][e]++;
+                  }
+              }
+            }
+        }
+        return seqAp;
+    }
+    public void printArray(int [][] seqAp){
+        final int senseAp=0;
+        final char fC = 'a';
+        final int botLinea=6;
+        int cont = 0;
+        for (int i = 0; i < seqAp.length;i++){
+            for (int e = 0; e < seqAp[0].length;e++){
+                if (seqAp[i][e]!= senseAp){
+                    if (cont == botLinea){
+                        cont = 0;
+                        System.out.println();
+                    }
+                    System.out.print(" | "+(char)(fC+i) + (char)(fC+e) + " "+seqAp[i][e]);
+                    cont++;
+                }
+
+            }
+        }
+    }
 }
